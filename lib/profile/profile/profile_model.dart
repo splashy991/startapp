94545080f9d94665a10872b63d8300d778efb8f0
splashy991 <130724338+splashy991@users.noreply.github.com>
2023-04-27 04:13:52 +0500

    Create profile_model.dart

---
+++ lib/profile/profile/profile_model.dart
@@ -0,0 +1,25 @@
+import '/auth/firebase_auth/auth_util.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+class ProfileModel extends FlutterFlowModel {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for dark widget.
+  bool? darkValue;
+
+  /// Initialization and disposal methods.
+
+  void initState(BuildContext context) {}
+
+  void dispose() {}
+
+  /// Additional helper methods are added here.
+
+}
